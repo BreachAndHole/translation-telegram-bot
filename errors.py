74420970c115d96dbe1817cb7c940e3fbf3f41e7
Sergey Frolov <frolovsu25@gmail.com>
2022-06-11 23:00:44 +0300

    Custom exceptions implemented

---
+++ errors.py
@@ -0,0 +1,10 @@
+class TranslationSearchError(Exception):
+    """ Error during translation link searching"""
+
+
+class TranslationConnectionError(Exception):
+    """ Error during connection """
+
+
+class CantGetTranslationError(Exception):
+    """ Error during translation extracting """
